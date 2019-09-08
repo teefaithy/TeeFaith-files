203d33afe4889e666d758ce51a9281ae8b881196
Oluwatosin Faith Kolawole <54301685+teefaithy@users.noreply.github.com>
2019-09-09 00:27:56 +0100

    Add files via upload

---
+++ Contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submitButton'])){
+	$name = $_POST['fullname'];
+	$mailFrom = $_POST['email'];
+	$title = $_POST['title'];
+	$message = $_POST['message'];
+	
+	$mailTo = "faitholuwatosin@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+	
+	mail(mailTo, $title, $txt, $headers);
+	header("Location: oluwatosin.html?mailsend");
+}
---
+++ index.php
@@ -0,0 +1,195 @@
+ <!DOCTYPE html>
+ <div class="box-2">
+ <html>
+ 		<head>
+ 			<title>Oluwatosin Faith Kolawole Resume</title> 
+<link rel="stylesheet" type="text/css" href="css/design.css">
+ 		</head>
+
+ <body>
+ 	<div class="container">
+	<h1>Oluwatosin Faith Kolawole</h1>
+	<h2><em>Freelance Web Content Writer</h2>
+	<h3>Lagos Nigeria</h3>
+	<h3>faitholuwatosin@gmail.com</em></h3>
+</div>
+
+<div style="text-align:right"><img src="https://res.cloudinary.com/teefaithy/image/upload/v1566601719/Profile_Picture_dsg3pb.jpg" width="180" height="auto" alt="Oluwatosin Faith Kolawole"></div>
+
+<div style="text-align:right">
+
+<h1>Skills</h1>
+<!-- Lists -->
+<ul>
+	<li>Writing (4 years)</li>
+	<li>Blog (4 years)</li>
+	<li>Wordpress (4 years)</li>
+	<li>SEO (3 years)</li>
+	<li>Editing (3 years)</li>	
+	<li>Google Docs and Sheets(3 years)</li>
+	<li>Keyword Research and Strategy (2 years)</li>
+	<li>Social Media Strategy (2 years)</li>
+	<li>Bios (2 years)</li>
+	<li>Content Marketing (Less than 1 year)</li>
+	<li>HTML (Less than 1 year)</li>
+	<li>CSS (Less than 1 year)</li>
+
+</div>
+
+
+	<!-- Paragraph -->
+		<div class="box-1">
+
+	<p>I am a 5th Year Medical Student and a Web Content Writer who has written a host of articles for the web, over the last 3 years, ranging from Health to Digital Marketing to random niches. I work on both simple and well-written blog posts and also well-researched and defined articles that are SEO-rich for the web. I am also certified in SEO Keyword Strategy and Content Marketing courses.</p> 
+	
+</div>
+
+<h2><strong>What You Will Gain From My Service:</strong></h2>
+
+<!-- Lists -->
+
+	<li>SEO</li>
+	<li>Keyword Research and Strategy</li>
+	<li>Content Planning, Creation and Analysis</li>
+	<li>Well-researched premium articles with backed-up facts</li>
+	<li>Befitting subheadings and bullet points to provide more details on the subject Quality and excellence</li>
+	<li>Easy and fast communication</li>
+	<li>Quick delivery within stipulated deadline</li>
+
+
+<!-- Paragraph -->
+<p><em>I put in my best to ensure that every client gets exactly what they requested and possibly more. I also never miss a deadline.</em></p>
+
+<h1><strong>Work Experience</strong></h1>
+
+<h2>Professional Web Content Writer and Developer</h2> 
+<h3>Content and Planning - Remote</h3>
+<h3>November 2018 to Present</h3>
+
+<!-- Paragraph -->
+<div class="box-1">
+
+<p>Content and Planning is a self-owned brand that focuses on helping brands find their voice and gain their grounds, with quality content, in their industry. 
+
+I create content on Instagram and Twitter via the social account <a href="http://instagram.com/content_plans" target="_blank"> @content_plans </a> and share some of them on <a href="https://www.linkedin.com/in/oluwatosinfaith/detail/recent-activity/posts/" target="_blank"> LinkedIn as articles </a>. The idea behind this is to let more people know about what I do and how I can render my service to them while putting them through basic things to help their brand survive. </p>
+
+</div>
+
+<h2>Digital Marketing Content Writer</h2>
+<h3>Digitimatic - Lagos, Nigeria</h3>
+<h3>November 2018 to January 2019</h3>
+
+<!-- Paragraph -->
+<div class="box-1">
+
+<p>I wrote well detailed and professional articles for Digital Marketing, following the description or outline provided.</p>
+
+</div>
+
+<h2>Scribe and Head of Content</h2>
+<h3>ISOKO Africa - Lagos, Nigeria</h3>
+<h3>November 2017 to January 2019</h3>
+
+<!-- Paragraph -->
+<div class="box-1">
+<p>I worked with a team of people known as The Scribes at ISOKO Africa, where we researched, wrote on and reviewed brands that promote Africa, indigenous or not. Eight months into working at ISOKO Africa, I was appointed as the Head of Content, where I managed the content team, including the Scribes, Blog Contributors, Editor, and Social Media Manager. </p>
+
+</div>
+
+<h2>Web Content Writer and Team Supervisor</h2>
+<h3>Vision Global - Kairaba Avenue, The Gambia</h3>
+<h3>November 2015 to December 2018</h3>
+
+<!-- Paragraph -->
+<div class="box-1">
+
+<p>I wrote and edited content for various websites, following any project at hand. During this period, I worked on thedroidreview.com, trainer.ae, stepbystep.com, realestateproperties.ca, hibusiness.ca, fireupfitness.com, fitness.site.uk, besttrainer.ae, foxglove.io, and kidbizo.com. I also led a team of content creators which included writers and editors and ensured a consistent effective output to promote the company's progress.</p>
+
+</div>
+
+<h1>Education</h1>
+
+<h2>MD in Medicine</h2>
+<h3>American International University West Africa - Kanifing, The Gambia</h3>
+<h3>May 2014 to December 2020</h3>
+
+
+<h1>Certifications/Licenses</h1>
+
+<h2>SEO: Keyword Strategy</h2>
+<h3>March 2019 to Present</h3>
+<h3><a href="https://drive.google.com/file/d/1WhVHSffJwNTOHxuXC7lyBEGTjBKKmOmC/view?usp=sharing" target="_blank" >Certified on LinkedIn</a></h3>
+
+<h2>Content Marketing Certified</h2>
+<h3>March 2019 to April 2021<h3>
+<h3><a href="https://drive.google.com/file/d/17LM3o_SlR0zm7sdxUknO41s8ORKW32hr/view?usp=sharing" target="_blank">Certified on HubSpot Academy</a></h3>
+
+<h2>Content Marketing: Social Media</h2>
+<h3>March 2019 to Present</h3>
+<h3><a href="https://drive.google.com/file/d/1DjER6-kYPcwN5B8siY5PPbw-zIhSQWMq/view" target="_blank">Certified on LinkedIn</a></h3>
+ 
+<h1>Assessments</h1>
+
+<h2>Search Engine Optimization — Familiar</h2>
+<h3>December 2018</h3>
+<!-- Pararaph -->
+<p>Measures a candidate's ability to interpret online website performance metrics and understand search engine optimization tactics.</p>
+<a href="https://share.indeedassessments.com/share_assignment/t6srskatwxgbxngj" target="_blank">Click here for full results</a>
+
+<h2>Social Media Skills — Highly Proficient</h2>
+<h3>December 2018</h3>
+<!-- Paragraph -->
+<p>Measures a candidate's ability to create content, communicate online, and build a brand's reputation.</p>
+<a href="https://share.indeedassessments.com/share_assignment/b-egahaa9yrlfmhc" target="_blank">Click here for full results</a>
+
+<br>
+<hr>
+<br>
+
+<form>
+        <table id="contactme" cellspacing="10">
+            <tr class="form-group">
+                <td>
+                    <h1>CONTACT ME</h1>
+                </td>
+            </tr>
+            <tr class="form-group">
+                <td>
+                    <input type="text" class="form-control" disabled="disabled" placeholder="To: Oluwatosin Faith Kolawole"/>
+                </td> 
+            </tr>
+            <tr class="form-group">
+                <td>
+                    <input type="text" name="fullname" class="form-control" id="fullname" placeholder="Enter Fullname" required="" />
+                    <p class="red hide" id="fullnameError">Name should be more than 4 characters</p>
+                </td>
+            </tr>
+            <tr class="form-group">
+                <td>
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Enter Email Address" required="" />
+                    <p class="red hide" id="emailError">Email is Invalid</p>
+                </td>
+            </tr>
+            <tr class="form-group">
+                <td>
+                    <input type="text" name="title" id="title" class="form-control" placeholder="Enter Title" required="" />
+                    <p class="red hide" id="titleError">Title should be more than 4 characters</p>
+                </td>
+            </tr>
+            <tr class="form-group">
+                <td>
+                    <textarea name="message" id="message" cols="30" rows="3" placeholder="Enter Message" class="form-control" required=""></textarea>
+                    <p class="red hide" id="messageError">Message should be more than 20 characters</p>
+                </td>
+            </tr>
+            <tr class="form-group">
+                <td>
+                    <button id="submitButton" class="btn-blue" onclick="validate()">Contact</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <script src="script.js"></script>
+    
+</div>
+</body>
